6249096e123a990bbf9d48ce5e5ddad3df50985e
18012961 <fortunaterefilwe@gmail.com>
2025-03-20 09:19:12 +0200

    removed footer for seamless design

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {fetchDataFromApi} from "./utils/api";
 import { useSelector, useDispatch } from 'react-redux';
 import { getApiConfiguration, getGenres } from './store/homeSlice';
 import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from './pages/searchResult/SearchResult';
@@ -68,7 +68,7 @@ const genresCall = async ()=>{
     <Route path='/explore/:mediaType' element={<Explore/>}/>
     <Route path='*' element={<PageNotFound/>}/>
   </Routes>
-  <Footer/>
+
   </BrowserRouter>)
 }
 
